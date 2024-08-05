d5b6b06a278096b66e96bf92e3c0eec33c6a6fe1
Shailendra Kumar <shailendra.kumar@svayamtech.com>
2024-08-05 17:24:55 +0530

    Update cron file

--- backend/cron.js
+++ backend/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://graphql-crash-course.onrender.com";
+const URL = "https://expense-tracker-2p33.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
